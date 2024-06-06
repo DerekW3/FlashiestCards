18c571259f964fd5c5435fa44e3796077b71097c
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-06-06 19:04:18 +0200

    Reformat: Propogate error instead from establish_db_connection

--- src/flashiestcards/backend.py
+++ src/flashiestcards/backend.py
@@ -1,11 +1,21 @@
 import sqlite3
 from datetime import datetime
+from sqlite3.dbapi2 import Cursor
 
 
-def establish_db_connection() -> sqlite3.Connection | None:
+def establish_db_connection() -> sqlite3.Connection:
     try:
-        connection = sqlite3.connect("flashcards.db")
-        return connection
+        db_connection: sqlite3.Connection = sqlite3.connect("flashcards.db")
+        return db_connection
     except sqlite3.Error as e:
         print(f"Error establishing database connection: {e}")
-        return None
+        raise e
+
+
+def create_flashcards_table():
+    try:
+        db_connection: sqlite3.Connection = establish_db_connection()
+        db_cursor: Cursor = db_connection.cursor()
+
+    except sqlite3.Error as e:
+        print(f"Error creating flashcards table: {e}")
