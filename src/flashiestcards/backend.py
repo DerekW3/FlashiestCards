d6ada940621a702727a2ef7581b6c1f6f6506cd5
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-06-06 19:05:24 +0200

    Fix: Use sqlite3 Cursor directly

--- src/flashiestcards/backend.py
+++ src/flashiestcards/backend.py
@@ -1,6 +1,5 @@
 import sqlite3
 from datetime import datetime
-from sqlite3.dbapi2 import Cursor
 
 
 def establish_db_connection() -> sqlite3.Connection:
@@ -15,7 +14,7 @@ def establish_db_connection() -> sqlite3.Connection:
 def create_flashcards_table():
     try:
         db_connection: sqlite3.Connection = establish_db_connection()
-        db_cursor: Cursor = db_connection.cursor()
+        db_cursor: sqlite3.Cursor = db_connection.cursor()
 
     except sqlite3.Error as e:
         print(f"Error creating flashcards table: {e}")
