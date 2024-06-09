ee2e47e82b2674da5051895b19b5d9158583e93a
DerekW3 <92753567+DerekW3@users.noreply.github.com>
2024-06-09 17:01:56 +0200

    Feat: Implement execute_sql_query function

--- src/flashiestcards/backend.py
+++ src/flashiestcards/backend.py
@@ -1,4 +1,5 @@
 import sqlite3
+from typing import Any
 
 
 def establish_db_connection() -> sqlite3.Connection:
@@ -29,3 +30,24 @@ def create_flashcards_table():
 
     except sqlite3.Error as e:
         print(f"Error creating flashcards table: {e}")
+
+
+def execute_sql_query(query, parameters=None) -> list[Any]:
+    try:
+        db_connection: sqlite3.Connection = establish_db_connection()
+        db_cursor: sqlite3.Cursor = db_connection.cursor()
+
+        if parameters:
+            db_cursor.execute(query, parameters)
+        else:
+            db_cursor.execute(query)
+
+        db_connection.commit()
+        results: list[Any] = db_cursor.fetchall()
+        db_cursor.close()
+
+        return results
+
+    except sqlite3.Error as e:
+        print(f"Error executing SQL query: {e}")
+        return []
